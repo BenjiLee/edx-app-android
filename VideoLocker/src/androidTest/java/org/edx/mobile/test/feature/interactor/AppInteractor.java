0337c5c555662b4f23707013d84f431f8d7085e2
bguertin <bguertin@edx.org>
2016-02-19 13:11:56 -0500

    sync onView

--- VideoLocker/src/androidTest/java/org/edx/mobile/test/feature/interactor/AppInteractor.java
+++ VideoLocker/src/androidTest/java/org/edx/mobile/test/feature/interactor/AppInteractor.java
@@ -3,9 +3,16 @@ package org.edx.mobile.test.feature.interactor;
 import android.app.Instrumentation;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
 
 import org.edx.mobile.base.MainApplication;
 import org.edx.mobile.module.prefs.PrefManager;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 
 public class AppInteractor {
     public LandingScreenInteractor launchApp() {
@@ -20,6 +27,7 @@ public class AppInteractor {
 
         instrumentation.startActivitySync(launchIntent);
         instrumentation.waitForIdleSync();
+        onView(isRoot()).check(matches(isDisplayed()));
 
         return new LandingScreenInteractor();
     }
